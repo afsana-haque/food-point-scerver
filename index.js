6c2b3300349fcbe39775d4afcc19a26d0a27bb7c
afsana-haque <afsana.haque3301@gmail.com>
2023-05-03 22:45:49 +0600

    added chefs json

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const port = process.env.PORT || 5000;
+
+const chefs = require("./data/chef.json");
+
+app.use(cors());
+
+app.get("/", (req, res) =>{
+    res.send("Food is running")
+});
+
+app.get("/chefs", (req, res) =>{
+    res.send(chefs);
+})
+
+app.listen(port, () => {
+    console.log(`food point API is running on port: ${port}`);
+})
